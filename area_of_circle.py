0303c5dd0449cfdd960481e4c5a45b20ef3bb70a
Vikas Kashyap <159269579+vikas-kashyap97@users.noreply.github.com>
2025-08-16 15:10:03 +0530

    Update area_of_circle.py

--- area_of_circle.py
+++ area_of_circle.py
@@ -4,3 +4,16 @@ def area(r):
 radius = float(input("Type your radius: "))
 result = area(radius)
 print(f"The area of the circle is {result}")
+
+
+def bigger_number(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+a = int(input("Number 1: "))
+b = int(input("Number 2: "))
+
+
+print("The bigger number is:", bigger_number(a, b))
